3c252ce218c95eb62237584570d0cbe0389e52af
Onur.Kerimoglu <onur.kerimoglu@uol.de>
2022-02-12 21:09:00 +0100

    RHSext_Cbased extended now to handle multiple phytoplankton; fabm-nflexpd_Cbased now split for IA and DA variants

--- src/RHSext_Cbased.F90
+++ src/RHSext_Cbased.F90
@@ -17,8 +17,26 @@
 
    implicit none
 
-   private
-!
+   public
+   
+   integer, parameter :: max_phynum = 10 !maximum number of phy. Adjust and recompile if necessary
+   
+   !structure to collect prey parameters (variable identifiers, stoichiometric ratios, etc)
+   type,public                          :: phy_pars
+     !type (type_state_variable_id)      :: id_?
+     type (type_dependency_id)          :: id_dep_vN_dQdt_I,id_dep_del_phyn_din,id_dep_fdinphy
+     !type (type_diagnostic_variable_id) :: id_?
+     !real(rk)                           :: ?
+     !logical                            :: ?
+   end type
+
+   !structure to collect phydata
+   type,public                         :: phy_data
+     real(rk),dimension(max_phynum)   :: f_din_phy
+     real(rk),dimension(max_phynum)   :: vN_dQdt_I
+     real(rk),dimension(max_phynum)   :: del_phyn_din
+   end type
+      
 ! !PUBLIC DERIVED TYPES:
    type,extends(type_base_model),public :: type_NflexPD_RHSext_Cbased
 !     Variable identifiers
@@ -26,9 +44,12 @@
       type (type_dependency_id)            :: id_dep_fdondin
       type (type_dependency_id)            :: id_dep_fdinphy
       type (type_dependency_id)            :: id_dep_del_phyn_din,id_dep_vN_dQdt_I
-      
+      type(phy_pars),dimension(:),allocatable :: phypar
+   
 !     Model parameters
-      logical  :: IA
+      logical  :: IA, DA
+      integer  :: num_phy
+      real(rk) :: kc
 
       contains
 
@@ -57,11 +78,17 @@
    class (type_NflexPD_RHSext_Cbased), intent(inout), target :: self
    integer,                        intent(in)            :: configunit
 !
+! !LOCAL VARIABLES
+!parameters of the self structure
+   integer                   :: i
+   character(len=2)          :: istr !i converted to character string
+!
 !EOP
 !-----------------------------------------------------------------------
 !BOC
    
    call self%get_parameter(self%IA, 'IA','-', 'whether IA approach is used', default=.true.)
+   call self%get_parameter(self%num_phy, 'numphy','-', 'number of phytoplankton instances to be coupled', default=2)
    
    ! Register state variables
    call self%register_state_variable(self%id_din,'din','mmolN/m^3','DIN concentration',     &
@@ -72,13 +99,23 @@
    
    call self%register_dependency(self%id_dep_fdondin, 'f_don_din','mmolN/m^3/d',    'Bulk N flux from DOM to DIM')
    
+   allocate(self%phypar(self%num_phy))
    if ( self%IA ) then !when using IA approach
      write(*,*)'  IA mode: ON'
-     call self%register_dependency(self%id_dep_del_phyn_din,'del_phyn_din', '-','Change in phyto-N per change in DIN')
-     call self%register_dependency(self%id_dep_vN_dQdt_I,'vN_dQdt_I', 'molN/m^3/d','phyC*(vN+delQ/delI*dI/dt)')
+     !call self%register_dependency(self%id_dep_del_phyn_din,'del_phyn_din', '-','Change in phyto-N per change in DIN')
+     !call self%register_dependency(self%id_dep_vN_dQdt_I,'vN_dQdt_I', 'molN/m^3/d','phyC*(vN+delQ/delI*dI/dt)')
+     DO i=1,self%num_phy
+      write (istr,'(i0)') i
+      call self%register_dependency(self%phypar(i)%id_dep_del_phyn_din,'del_phyn_din'//trim(istr), 'mmolN/m^3/s','Change in phyto-N per change in DIN for phy-'//trim(istr))
+       call self%register_dependency(self%phypar(i)%id_dep_vN_dQdt_I,'vN_dQdt_I'//trim(istr), 'mmolN/m^3/s','phyC*(vN+delQ/delI*dI/dt)_for_phy'//trim(istr))
+     END DO
    else
      write(*,*)'  IA mode: OFF'
-     call self%register_dependency(self%id_dep_fdinphy,'f_din_phy', 'mmolN/m^3/d','f_din_phy')
+     !call self%register_dependency(self%id_dep_fdinphy,'f_din_phy', 'mmolN/m^3/d','f_din_phy')
+     DO i=1,self%num_phy
+      write (istr,'(i0)') i
+      call self%register_dependency(self%phypar(i)%id_dep_fdinphy,'f_din_phy'//trim(istr), 'mmolN/m^3/s','flux_between_din_and_phy'//trim(istr))
+     END DO
    end if  
    end subroutine initialize
 !EOC
@@ -98,25 +135,44 @@
 ! !LOCAL VARIABLES:
    real(rk)                   :: f_don_din
    real(rk)                   :: total_del_phyn_din,vN_dQdt_I
-   real(rk)                   :: f_din_phy
+   real(rk)                   :: f_din_phy_sum,vN_dQdt_I_sum,del_phyn_din_sum
+   integer                    :: i
+   character(len=2)          :: istr !i converted to character string
    real(rk), parameter        :: secs_pr_day = 86400.0_rk
+   type(phy_data)             :: phydat
 !EOP
 !-----------------------------------------------------------------------
 !BOC
    ! Enter spatial loops (if any)
    _LOOP_BEGIN_
-
+   
+   phydat%vN_dQdt_I=0.0
+   phydat%del_phyn_din=0.0
+   phydat%f_din_phy=0.0
+   
    _GET_(self%id_dep_fdondin,f_don_din)
    
+   !Collect the data from coupling targets
+   DO i=1,self%num_phy
+     write (istr,'(i0)') i
+     if ( self%IA ) then !when using IA approach
+       _GET_(self%phypar(i)%id_dep_vN_dQdt_I,phydat%vN_dQdt_I(i))
+       _GET_(self%phypar(i)%id_dep_del_phyn_din,phydat%del_phyn_din(i))
+       !write(*,*)'i,vN_dQdt_I,del_phyn_din',i,phydat%vN_dQdt_I(i),phydat%del_phyn_din(i)
+     else
+       _GET_(self%phypar(i)%id_dep_fdinphy,phydat%f_din_phy(i))
+       !write(*,*)'i,f_din_phy',phydat%f_din_phy(i)
+     end if
+   END DO
+       
+   !Set the ODEs    
    if ( self%IA ) then !when using IA approach
-     _GET_(self%id_dep_del_phyn_din,total_del_phyn_din)
-     _GET_(self%id_dep_vN_dQdt_I,vN_dQdt_I) !vN_dQdt = (vN+delQ_delI*dI_dt)*phyC
-     !_SET_ODE_(self%id_din, - (vN+delQ_delI*dI_dt)*phyC/(1+total_del_phyn_din))
-     !_SET_ODE_(self%id_din,   (f_don_din )/(1.0+total_del_phyn_din)) ! (eq A-8)
-     _SET_ODE_(self%id_din, (f_don_din/secs_pr_day - vN_dQdt_I/secs_pr_day)/(1+total_del_phyn_din))
+     vN_dQdt_I_sum=sum(phydat%vN_dQdt_I)
+     del_phyn_din_sum=sum(phydat%del_phyn_din)
+     _SET_ODE_(self%id_din, (f_don_din/secs_pr_day - vN_dQdt_I_sum/secs_pr_day)/(1+del_phyn_din_sum))
    else
-     _GET_(self%id_dep_fdinphy,f_din_phy)
-     _SET_ODE_(self%id_din, f_don_din/secs_pr_day-f_din_phy/secs_pr_day)
+     f_din_phy_sum=sum(phydat%f_din_phy)  
+     _SET_ODE_(self%id_din, f_don_din/secs_pr_day-f_din_phy_sum/secs_pr_day)
    end if
    
    ! Leave spatial loops (if any)
